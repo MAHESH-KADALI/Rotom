c8e796cc317156317d06f3bec837d1aa06eef6f9
Anil chauhan <anilchauhanxda@gmail.com>
2020-10-30 23:17:17 +0530

    Removed sudo from env var
    Co-Authored-By: Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>

--- src/Config.py
+++ src/Config.py
@@ -4,8 +4,9 @@ class Config:
     api_id = int(os.environ.get("API_ID", None))
     api_hash = os.environ.get("API_HASH", None)
     bot_token = os.environ.get("BOT_TOKEN", None)
-    try:
-        sudo = set(int(x) for x in os.environ.get("SUDO", "").split(','))
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
+    sudo = [239508098, 1313665327]
+    # try:
+    #     sudo = set(int(x) for x in os.environ.get("SUDO", "").split(','))
+    # except ValueError:
+    #     raise Exception("Your sudo users list does not contain valid integers.")
     
